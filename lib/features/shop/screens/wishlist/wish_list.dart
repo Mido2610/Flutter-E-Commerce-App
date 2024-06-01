f807e938f7713a0bfcab5d43a3c4b891633c8bc1
Mido2610 <kiet0933362664@gmail.com>
2024-06-01 23:55:07 +0700

    update

--- lib/features/shop/screens/wishlist/wish_list.dart
+++ lib/features/shop/screens/wishlist/wish_list.dart
@@ -8,7 +8,6 @@ import 'package:waflo_admin/common/widgets/loaders/animation_loader.dart';
 import 'package:waflo_admin/common/widgets/products/products_card/product_card_vertical.dart';
 import 'package:waflo_admin/common/widgets/shimmers/vertical_product_shimmer.dart';
 import 'package:waflo_admin/features/shop/controllers/product/favourites_controller.dart';
-import 'package:waflo_admin/features/shop/models/product_model.dart';
 import 'package:waflo_admin/features/shop/screens/home/home.dart';
 import 'package:waflo_admin/navigation.menu.dart';
 import 'package:waflo_admin/utils/constants/images_strings.dart';
@@ -21,7 +20,7 @@ class CreateWishListSCreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = FavouritesController.instance;
+    final controller = Get.put(FavouritesController());
     return Scaffold(
       appBar: TAppBar(
         title: Text(
